d73dbc6d3543158ee1d41c7fd1af46a3712d1eec
Taner Duman <tanerduman@outlook.com>
2023-05-20 01:42:34 +0300

    first_commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+  console.log(firstName,lastName,email);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
